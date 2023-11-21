2258e0c8300d2e28ce398de788fa966d7f5dd72d
chernyshovdmitrydev <148445915+chernyshovdmitrydev@users.noreply.github.com>
2023-11-21 14:59:11 +0400

    moire

---
+++ src/components/Base/BaseHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- eslint-disable  -->
+  <header class="header container">
+    <div class="header__wrapper">
+      <router-link class="header__info" :to="{ name: 'main' }">
+        Каталог
+      </router-link>
+      <router-link  class="header__logo" :to="{ name: 'main' }">
+        <a class="header__logo" href="#">
+        <img src="img/svg/logo-moire.svg" alt="Логотип интернет магазина Moire" width="116" height="34">
+      </a>
+      </router-link>
+      <a class="header__tel" href="tel:8 800 600 90 09">
+        8 800 600 90 09
+      </a>
+      <!-- Иконка корзины с кол-вом товаров -->
+      <CartIndicator />
+    </div>
+  </header>
+</template>
+
+<script>
+import CartIndicator from '../Cart/CartIndicator.vue';
+
+export default {
+  components: { CartIndicator },
+};
+</script>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- eslint-disable  -->
+  <div>
+    <BaseHeader>
+      <slot name="header"></slot>
+    </BaseHeader>
+    <slot></slot>
+    <BaseFooter>
+      <slot name="footer"></slot>
+    </BaseFooter>
+  </div>
+</template>
+
+<script>
+import BaseHeader from '@/components/Base/BaseHeader.vue';
+import BaseFooter from '@/components/Base/BaseFooter.vue';
+
+export default {
+  components: { BaseHeader, BaseFooter },
+};
+</script>
